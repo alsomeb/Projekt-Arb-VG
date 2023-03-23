a5c2f258e56886811ba806ac1b663cc541a401fe
alsomeb <alsomeb@gmail.com>
2023-03-23 21:21:22 +0100

    - removed animation.js and occurences
    - Created cart.html + cart.css
    
    - Responsive design cart
    
    - All html pages tested in validator, passed

--- js/animation.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $(".cart").click(function () {
-    let div = $(".animation");
-    div.animate({ fontSize: "1.5em" }, "slow");
-    div.animate({ fontSize: "1em" }, "slow");
-  });
-});
