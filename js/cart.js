9287b8e139e2e73edf032da16c773719861d3ad7
alsomeb <alsomeb@gmail.com>
2023-03-24 22:30:23 +0100

    cart fixed, not done though

--- js/cart.js
+++ js/cart.js
@@ -10,13 +10,14 @@ const data = await getAllProductsBackUp();
 
 const renderCartUi = async () => {
   const cardSection = document.querySelector(".todo-cards");
+  let totalSum = calcCurrentTotalSum();
 
   if (currentCart != null) {
     // TODO
     cardSection.innerHTML = `
     <h1 class="text-center h1">Review your cart</h1>
-    <div class="total-cart text-center h2 my-5">Total cart: -$</div>
-    <div class="row">
+    <div class="total-cart text-center h2 mt-5">Total cart: ${totalSum} $</div>
+    <div class="row" id="cardsrow">
     </div>
     <div class="text-center">
     <a href="#" class="btn btn-secondary my-5">Proceed to Checkout</a>
@@ -27,14 +28,83 @@ const renderCartUi = async () => {
   }
 };
 
-renderCartUi();
+const findProductById = (id) => {
+  return data.find((element) => element.id === id);
+};
 
-const calcCurrentSum = () => {
+const calcCurrentTotalSum = () => {
+  let sum = 0;
+
   currentCart.forEach((element) => {
     let itemId = element.id;
     let itemAmount = element.amount;
     console.log("ID: " + itemId + " AMOUNT: " + itemAmount);
+    sum += findProductById(itemId).price * itemAmount;
   });
+
+  return sum.toFixed(1);
 };
 
-calcCurrentSum();
+const getFullInfoProductArray = () => {
+  let array = [];
+
+  currentCart.forEach((element) => {
+    let itemId = element.id;
+    let itemAmount = element.amount;
+    const product = findProductById(itemId);
+    for (let index = 0; index < itemAmount; index++) {
+      array.push(product);
+    }
+  });
+
+  return array;
+};
+
+const handleRenderCards = () => {
+  let html = "";
+  let cards = getFullInfoProductArray();
+  const cardsRowElement = document.getElementById("cardsrow");
+  console.log(cardsRowElement);
+  cards.forEach((element) => {
+    console.log(element);
+    let htmlSegment = `
+    <div class="todo-column col-lg-4 col-md-6">
+    <div class="card">
+      <div class="card-header fw-bold text-center">${element.category}</div>
+      <div class="card-body text-center">
+        <div class="img-container mt-4">
+          <img
+            class="w-25 rounded"
+            src="${element.image}"
+            alt="product img"
+          />
+        </div>
+        <p class="lead my-4 fs-6">
+          ${element.title}
+        </p>
+        <div class="total-price fs-6 fw-bold">Total: todo$</div>
+        <div class="amount fs-6 fw-bold">todo</div>
+      </div>
+      <div class="card-footer">
+        <div class="update-buttons">
+          <div class="box">
+            <button class="increase btn btn-outline-secondary">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+            <button class="increase btn btn-outline-secondary">
+              <i class="fa-solid fa-minus"></i>
+            </button>
+            <a class="btn btn-outline-danger" href="#">Delete</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+    html += htmlSegment;
+  });
+  cardsRowElement.innerHTML = html;
+};
+
+renderCartUi();
+handleRenderCards();
