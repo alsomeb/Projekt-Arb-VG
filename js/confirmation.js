6108c34a6172b251cca1e10c0f5769f347c4609d
alsomeb <alsomeb@gmail.com>
2023-03-28 11:22:12 +0200

    - added getFullInfoProductArray() and findProductById() in confirmation.js

--- js/confirmation.js
+++ js/confirmation.js
@@ -5,7 +5,27 @@ handleSetCartAmount();
 const currentCart = JSON.parse(localStorage.getItem("cart"));
 const data = await getAllProductsBackUp();
 
+// Återanvänder findProductById() & getFullInfoProductArray() för confirmation.html
+
 // Finds specific product
 const findProductById = (id) => {
   return data.find((element) => element.id === id);
 };
+
+// With LocalStorage 'cart' key we can find id and amount of product, then we can fetch full info regarding products and collect them to one array
+const getFullInfoProductArray = () => {
+  let array = [];
+
+  currentCart.forEach((element) => {
+    let itemId = element.id;
+    let itemAmount = element.amount;
+    const product = findProductById(itemId);
+    product.amount = itemAmount; // lägger till amount property på befintlig objekt så vi vet hur många det rör sig om i kundvagnen, blir lättare också att räkna ut summa per produkt!
+    array.push(product);
+  });
+
+  return array;
+};
+
+const fullArray = getFullInfoProductArray();
+console.log(fullArray);
